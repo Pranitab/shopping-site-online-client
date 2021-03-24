36ac3f0ece497a2a1a40c56a5d6d6110be37661e
Pranita Bhagat <Pranitabhagat91@gmail.com>
2021-03-24 12:59:05 -0700

    added build

---
+++ build/precache-manifest.86fcbc0b3f33ce6263aa1efa409725ad.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56ec306aedcc19ff6d2e45ed834a3885",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9e43184254ec0901fba",
+    "url": "/static/css/2.e6f51e71.chunk.css"
+  },
+  {
+    "revision": "dc02678b3c91ef967792",
+    "url": "/static/css/main.55be3c40.chunk.css"
+  },
+  {
+    "revision": "c9e43184254ec0901fba",
+    "url": "/static/js/2.b4dfc254.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b4dfc254.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc02678b3c91ef967792",
+    "url": "/static/js/main.e263cb48.chunk.js"
+  },
+  {
+    "revision": "976bc2dd75b6c4282e2e",
+    "url": "/static/js/runtime-main.e0f5a793.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
